9073bcdf501ba9ef57690931d1fbfd12d986deca
SheryAtef <SheryAtef95@gmail.com>
2017-07-18 14:46:49 +0200

    reform the application all right till the end of the css part

--- attendance/views.py
+++ attendance/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Admission
+from.models import Person
+from django.utils import timezone
 
 def admission_list(request):
-    return render(request, 'attendance/admission_list.html', {})
+     admissions = Admission.objects.filter(lastAttendance__lte=timezone.now()).order_by('lastAttendance')
+     return render(request, 'attendance/admission_list.html',{'admissions':admissions})
 # Create your views here.
